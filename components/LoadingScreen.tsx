9482532cbe72855e6734875e9603e2af2d446e77
Trappeew1ch <Trappeew1ch@gmail.com>
2026-01-27 20:18:59 +0600

    fix: update loading screen image and spinner position

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 export const LoadingScreen: React.FC = () => {
     return (
-        <div 
+        <div
             className="fixed inset-0 z-[100] flex flex-col items-center justify-center bg-[#09090b]"
             style={{
-                backgroundImage: `url('/images/loadingpage.jpg')`,
+                backgroundImage: `url('/images/loading-new.jpg')`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat'
             }}
         >
-            {/* Loading spinner positioned below center */}
-            <div className="absolute" style={{ top: '65%' }}>
+            {/* Loading spinner positioned higher */}
+            <div className="absolute" style={{ top: '45%' }}>
                 <div className="relative">
                     {/* Outer glow */}
                     <div className="absolute inset-0 w-12 h-12 bg-blue-500/30 rounded-full blur-xl animate-pulse"></div>
-                    
+
                     {/* Spinner */}
                     <div className="w-12 h-12 border-4 border-white/20 border-t-white rounded-full animate-spin"></div>
                 </div>
