337cb5c3f2b438b017068268cf2624f4b0f4b09c
Trappeew1ch <Trappeew1ch@gmail.com>
2026-02-05 13:56:02 +0600

    fix: build error in EarningsView

--- components/EarningsView.tsx
+++ components/EarningsView.tsx
@@ -73,8 +73,8 @@ export const EarningsView: React.FC<EarningsViewProps> = ({ onNavigate }) => {
     };
 
     const formatNumber = (num: number): string => {
-        if (num >= 1000000) return `${ (num / 1000000).toFixed(1) } M`;
-        if (num >= 1000) return `${ (num / 1000).toFixed(1) } K`;
+        if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
+        if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
         return num.toString();
     };
 
